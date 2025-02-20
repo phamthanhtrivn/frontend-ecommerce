b489589f5e9beb33ae4280b4240a34763bbdd11c
Tri Pham <phamthanhtri0712@gmail.com>
2025-02-20 15:34:50 +0700

    Login Page

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -6,7 +6,7 @@ import { ShopContext } from "../../context/ShopContext";
 function Navbar() {
   const [visible, setVisible] = useState(false);
 
-  const { setShowSearch, getCartCount } = useContext(ShopContext)
+  const { setShowSearch, getCartCount } = useContext(ShopContext);
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
@@ -38,14 +38,16 @@ function Navbar() {
           src={assets.search_icon}
           alt="search_icon"
           className="w-5 cursor-pointer"
-          onClick={() => setShowSearch(true)} 
+          onClick={() => setShowSearch(true)}
         />
         <div className="group relative">
-          <img
-            src={assets.profile_icon}
-            alt="profile_icon"
-            className="w-5 cursor-pointer"
-          />
+          <Link to='/login'>
+            <img
+              src={assets.profile_icon}
+              alt="profile_icon"
+              className="w-5 cursor-pointer"
+            />
+          </Link>
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My Profile</p>
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,61 @@
+import { useState } from "react";
+
 function Login() {
+  const [currentState, setCurrentState] = useState("Sign up");
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Login</div>
-  )
+    <form onSubmit={onSubmitHandler} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="prata-regular text-3xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+      </div>
+      {currentState === "Login" ? (
+        ""
+      ) : (
+        <input
+          type="text"
+          className="w-full px-3 py-2 border border-gray-600"
+          placeholder="Họ và Tên..."
+          required
+        />
+      )}
+      <input
+        type="email"
+        className="w-full px-3 py-2 border border-gray-600"
+        placeholder="Email..."
+        required
+      />
+      <input
+        type="password"
+        className="w-full px-3 py-2 border border-gray-600"
+        placeholder="Mật khẩu..."
+        required
+      />
+      <div className="w-full flex justify-between text-sm mt-[-8px]">
+        <p className="cursor-pointer">Quên mật khẩu?</p>
+        {currentState === "Login" ? (
+          <p
+            onClick={() => setCurrentState("Sign up")}
+            className="cursor-pointer"
+          >
+            Tạo tài khoản
+          </p>
+        ) : (
+          <p
+            onClick={() => setCurrentState("Login")}
+            className="cursor-pointer"
+          >
+            Đăng nhập
+          </p>
+        )}
+      </div>
+      <button className="bg-black text-white font-light px-8 py-2 mt-4">{currentState === 'Login' ? 'Đăng nhập' : 'Đăng kí'}</button>
+    </form>
+  );
 }
 
-export default Login
+export default Login;
